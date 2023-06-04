db7250e0b45317138ea3113046d31bb73c8ee992
MattGomb <gombosonline@gmail.com>
2023-06-04 12:45:10 -0500

    Update context with type and data for name and thumbnail

--- src/context/SearchContextProvider.tsx
+++ src/context/SearchContextProvider.tsx
@@ -1,45 +1,50 @@
-"use client"
-import React, { ReactNode, useEffect, useState } from 'react'
-import { searchContext } from './SearchContext'
-import { IStrengths } from '@/assets/models'
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
+import { searchContext } from "./SearchContext";
+import { IStrengths, IPersonData } from "@/assets/models";
 
 interface SearchContextProviderProps {
-	children: ReactNode
+  children: ReactNode;
 }
 
-const SearchContextProvider = ({children}: SearchContextProviderProps) => {
-	const [username, setUsername] = useState('')
-	const [data, setData] = useState<IStrengths[]>([]);
-	
-	const fetchSkills = async (username: string = "torrenegra") => {
-		const res = await fetch(`http://localhost:5000/data/${username}`);
-		const data = await res.json();
-		let strengths: IStrengths[] = [];
-		data.strengths.forEach((item: IStrengths) =>
-			strengths.push(item )
-		);
-		setData(strengths);
-	};
+const SearchContextProvider = ({ children }: SearchContextProviderProps) => {
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState<IStrengths[]>([]);
+	const [personData, setPersonData] = useState<IPersonData>({
+    name: "Alexander Torrenegra",
+    pictureThumbnail:
+      "https://res.cloudinary.com/torre-technologies-co/image/upload/c_fill,h_150,w_150/v0/origin/starrgate/users/profile_bd307a3ec329e10a2cff8fb87480823da114f8f4.jpg",
+  });
 
-	useEffect(() => {
-		fetchSkills(username);
-	}, []);
+  const fetchSkills = async (username: string = "torrenegra") => {
+    const res = await fetch(`http://localhost:5000/data/${username}`);
+    const data = await res.json();
+    let strengths: IStrengths[] = [];
+    data.strengths.forEach((item: IStrengths) => strengths.push(item));
+		setPersonData(data.person);
+    setData(strengths);
+  };
 
-	const handleUsername = (value: string) => {
-		setUsername(value)
-		
-	}
+  useEffect(() => {
+    fetchSkills(username);
+  }, []);
 
-	const contextValue = {
-		username,
-		handleUsername
-	}
+  const handleUsername = (value: string) => {
+    setUsername(value);
+  };
 
-	return (
-		<searchContext.Provider value={{username, handleUsername, data, fetchSkills}}>
-			{children}
-		</searchContext.Provider>
-	)
-}
+  const contextValue = {
+    username,
+    handleUsername,
+  };
 
-export default SearchContextProvider
+  return (
+    <searchContext.Provider
+      value={{ username, handleUsername, data, personData, fetchSkills }}
+    >
+      {children}
+    </searchContext.Provider>
+  );
+};
+
+export default SearchContextProvider;
